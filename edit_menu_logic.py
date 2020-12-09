ee14aa96c95c83120e82f007186ea3c4a0d3852d
Samuel <samuelfoley@live.ie>
2020-12-09 21:59:37 +0000

    Alter screen fix

--- edit_menu_logic.py
+++ edit_menu_logic.py
@@ -8,7 +8,7 @@ def eventLoop(window, event, values):
     file = "databases/screenings_db.txt"
 
     if event == 'Main Menu':
-        window['-MOVIES-'].update(values=utils.get_view_list("screening",file))
+        window['-MOVIES-'].update(values=utils.get_view_list("movies",file))
         backToMenu()
     if event == 'Save':
         utils.save_to_file(file, window['-MOVIES-'].get_list_values())
