0f0d26bc09a2114117fbccdfda3454a7b46f405d
Samuel <samuelfoley@live.ie>
2020-12-09 16:22:57 +0000

    "Merged Issue"w

--- screenings_ui.py
+++ screenings_ui.py
@@ -1,7 +1,11 @@
 import PySimpleGUI as sg
 import ui_utils
+from listings import ListFactory
 
 Heading = "TheAter Screenings"
+list_factory = ListFactory()
+screeningList = list_factory.create_list("databases/screenings_db.txt")
+screeningsInfo = screeningList.get_view_list(screeningList.filename)
 
 screeningsInfo = ui_utils.get_view_list("databases/screenings_db.txt")
 
