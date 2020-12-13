99e0fa7ebf22faf50d4ec17e795537e86ded2883
Lennaayy <lennyeum@gmail.com>
2020-12-13 17:04:59 +0000

    Password protection & UI improvements

--- ui_controller.py
+++ ui_controller.py
@@ -54,38 +54,38 @@ class UI_Controller:
     
     def open_screening_ui(self):
         if self._screenings_ui == None:
-            self._screenings_ui = sg.Window(screenings_ui.Heading, screenings_ui.userLayout,size=(600,230), finalize=True)
+            self._screenings_ui = sg.Window(screenings_ui.Heading, screenings_ui.userLayout, finalize=True)
         self._current_ui = self._screenings_ui
 
     def open_concessions_ui(self):
         if self._concessions_ui == None:
-            self._concessions_ui = sg.Window(concessions_ui.Heading, concessions_ui.userLayout,size=(600,250), finalize=True)
+            self._concessions_ui = sg.Window(concessions_ui.Heading, concessions_ui.userLayout, finalize=True)
         self._current_ui = self._concessions_ui
     
     def open_ticket_sales_ui(self):
         if self._ticket_sales_ui == None:
-            self._ticket_sales_ui = sg.Window(ticket_sales_ui.Heading, ticket_sales_ui.adminLayout,size=(600,200), finalize=True)
+            self._ticket_sales_ui = sg.Window(ticket_sales_ui.Heading, ticket_sales_ui.adminLayout, finalize=True)
         self._current_ui = self._ticket_sales_ui
 
     def open_concession_sales_ui(self):
         if self._concession_sales_ui == None:
-            self._concession_sales_ui = sg.Window(concession_sales_ui.Heading, concession_sales_ui.adminLayout,size=(600,200), finalize=True)
+            self._concession_sales_ui = sg.Window(concession_sales_ui.Heading, concession_sales_ui.adminLayout, finalize=True)
         self._current_ui = self._concession_sales_ui
 
     def open_book_ticket_ui(self, movie):
-        self._book_ticket_ui = sg.Window(book_ticket_ui.Heading, book_ticket_ui.showLayout(movie), size=(600,200), finalize=True)
+        self._book_ticket_ui = sg.Window(book_ticket_ui.Heading, book_ticket_ui.showLayout(movie), finalize=True)
         self._current_ui = self._book_ticket_ui
 
     def open_purchase_ticket_ui(self, movie):
-        self._purchase_ticket_ui = sg.Window(purchase_ticket_ui.Heading, purchase_ticket_ui.showLayout(movie), size=(600,200), finalize=True)
+        self._purchase_ticket_ui = sg.Window(purchase_ticket_ui.Heading, purchase_ticket_ui.showLayout(movie), finalize=True)
         self._current_ui = self._purchase_ticket_ui
     
     def open_redeem_booking_ui(self):
-        self._redeem_booking_ui = sg.Window(redeem_booking_ui.Heading, redeem_booking_ui.showLayout(), size=(600,250), finalize=True)
+        self._redeem_booking_ui = sg.Window(redeem_booking_ui.Heading, redeem_booking_ui.showLayout(), finalize=True)
         self._current_ui = self._redeem_booking_ui
 
     def open_review_bookings_loop(self):
-        self._review_booking_ui = sg.Window(review_bookings_ui.Heading, review_bookings_ui.showLayout(), size=(600,200), finalize=True)
+        self._review_booking_ui = sg.Window(review_bookings_ui.Heading, review_bookings_ui.showLayout(), finalize=True)
         self._current_ui = self._review_booking_ui
 
     def open_edit_screenings_ui(self):
