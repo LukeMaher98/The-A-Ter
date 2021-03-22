fe7be6684caccc37a9f99e7c403bea1b30c068f4
Samuel Foley <samuelfoley@live.ie>
2021-03-22 21:14:15 +0000

    Adding Command Design Pattern for Command
    - Added command design pattern for the log in functionality
    - remove some not needed login code
    - changed my login in db for easy testing

--- logic_pkg/entry_logic.py
+++ logic_pkg/entry_logic.py
@@ -1,6 +1,56 @@
 from controllers import ui_controller, logic_controller
 import requests
+import abc
 
+class Invoker:
+    def __init__(self):
+        self._commands = []
+
+    def store_command(self, command):
+        self._commands.append(command)
+
+    def execute_commands(self):
+        for command in self._commands:
+            command.execute()
+
+class Command(metaclass=abc.ABCMeta):
+    def __init__(self, login):
+        self._login = login
+
+    @abc.abstractmethod
+    def execute(self):
+        pass
+
+class LoginCommand(Command) :   
+
+    def execute(self) :
+        self._login.Login()
+    
+
+class User_Login :
+    def __init__(self, userName):
+        self._userName = userName
+
+    def Login(self):
+        ui_controller.ui.get_current_ui().Hide()
+        ui_controller.ui.open_main_menu_user_ui()
+        ui_controller.ui.set_current_user(self._userName)
+        logic_controller.logic.set_current_user(self._userName)
+        logic_controller.logic.set_main_menu_user_loop()
+        logic_controller.logic.set_auth_type("user")
+
+    
+class Admin_Login : 
+    def __init__(self, userName):
+        self._userName = userName
+
+    def Login(self):
+        ui_controller.ui.get_current_ui().Hide()
+        ui_controller.ui.open_main_menu_admin_ui()
+        ui_controller.ui.set_current_user(self._userName)
+        logic_controller.logic.set_current_user(self._userName)
+        logic_controller.logic.set_main_menu_admin_loop()
+        logic_controller.logic.set_auth_type("admin")    
 
 def loginEventLoop(window, event, values):
     window['-USERNAME-'].update("")
@@ -43,30 +93,6 @@ def signupEventLoop(window, event, values):
     window['-PASSWORD-'].update("")
     window['-CONFIRM-'].update("")
 
-def adminLogin(username):
-    requests.post("https://logs-01.loggly.com/inputs/990e729b-d1a0-4ad1-a774-78d9c11a93c7/tag/http/", json={
-        "login": "true",
-        "AccountType":"Admin"
-    })
-    ui_controller.ui.get_current_ui().Hide()
-    ui_controller.ui.open_main_menu_admin_ui()
-    ui_controller.ui.set_current_user(username)
-    logic_controller.logic.set_current_user(username)
-    logic_controller.logic.set_main_menu_admin_loop()
-    logic_controller.logic.set_auth_type("admin")
-
-def userLogin(username):
-    requests.post("https://logs-01.loggly.com/inputs/990e729b-d1a0-4ad1-a774-78d9c11a93c7/tag/http/", json={
-        "login": "true",
-        "AccountType":"Customer"
-    })
-    ui_controller.ui.get_current_ui().Hide()
-    ui_controller.ui.open_main_menu_user_ui()
-    ui_controller.ui.set_current_user(username)
-    logic_controller.logic.set_current_user(username)
-    logic_controller.logic.set_main_menu_user_loop()
-    logic_controller.logic.set_auth_type("user")
-
 def goToLogin():
     ui_controller.ui.get_current_ui().Hide()
     ui_controller.ui.open_login_ui()
@@ -85,9 +111,17 @@ def validateLogin(window, username, password):
     if username in usernames:
         if password == passwords[usernames.index(username)]:
             if authTypes[usernames.index(username)] == "admin":
-                adminLogin(username)
+                admin_login = Admin_Login(username)
+                login_command = LoginCommand(admin_login)
+                invoker = Invoker()
+                invoker.store_command(login_command)
+                invoker.execute_commands()
             else:
-                userLogin(username)
+                user_login = User_Login(username)
+                login_command = LoginCommand(user_login)
+                invoker = Invoker()
+                invoker.store_command(login_command)
+                invoker.execute_commands()
         else:
             window['-OUTPUT-'].update("Entered password is invalid")
             requests.post("https://logs-01.loggly.com/inputs/990e729b-d1a0-4ad1-a774-78d9c11a93c7/tag/http/", json={
