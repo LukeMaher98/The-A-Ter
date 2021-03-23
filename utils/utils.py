ab7719e3d858f369f51e78d6af6c28d433656ea4
C_Frohburg <17237823@studentmail.ul.ie>
2021-03-23 21:45:44 +0000

    Intercepts the creation if new users to encrypt passwords before writing to db

--- utils/utils.py
+++ utils/utils.py
@@ -165,3 +165,34 @@ def deleteSelected(delete, values):
             output.append(v)
 
     return output   
+
+plainArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+encrArr = ["x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+            "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
+
+def encrypt(password):
+
+    newpassword = ""
+    for element in password:
+        try:
+            i = plainArr.index(element)
+        except:
+            newpassword += element
+        else:
+            newpassword += encrArr[i]
+
+    return newpassword 
+
+def decrypt(password):
+
+    newpassword = ""
+    for element in password:
+        try:
+            i = encrArr.index(element)
+        except:
+            newpassword += element
+        else:
+            newpassword += plainArr[i]
+
+    return newpassword 
